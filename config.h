2828002d41b6da0159df6a4e5853d415b101ed87
Srivathsan Murali <sri@vathsan.com>
2019-09-10 18:35:10 +0200

    molokai colorscheme

--- config.h
+++ config.h
@@ -88,42 +88,37 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
+	"#121212",  // color0
+	"#fa2573",  // color1
+	"#98e123",  // color2
+	"#dfd460",  // color3
+	"#1080d0",  // color4
+	"#8700ff",  // color5
+	"#43a8d0",  // color6
+	"#bbbbbb",  // color7
+	"#555555",  // color8
+	"#f6669d",  // color9
+	"#b1e05f",  // color10
+	"#fff26d",  // color11
+	"#00afff",  // color12
+	"#af87ff",  // color13
+	"#51ceff",  // color14
+	"#ffffff",  // color15
 	[255] = 0,
-
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	"#121212", // 256 -> background
+	"#bbbbbb", // 257 -> foreground
+	"#bbbbbb", // 258 -> cursor
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
